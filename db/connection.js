7618c8592d8016dc777ff814ed62f2640ef8d955
Torreythms <torreythms@gmail.com>
2020-04-16 18:59:24 -0400

    added the connection js file with updated code laying out the promises and proper routes to connect to the database

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,23 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/countries", {useNewUrlParser:  true});
+mongoose.Promise = Promise;
+
+let mongoURI = ""; // setting the uri to connect to our local mongo host
+
+if (process.env.NODE_ENV === "production"){
+    mongoURI =  process.env.DB_URL;
+} else {
+    mongoURI = "mongodb://localhost/countryAPI";
+}
+
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useFindAndModify: false })
+  .then(instance =>
+    console.log(`Connected to db: ${instance.connections[0].name}`)
+  )
+  .catch(error => console.log("Connection failed!", error));
+
+
 
 module.exports = mongoose;
